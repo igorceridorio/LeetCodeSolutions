992c5263fd511baf64ad7aff5b8b2cdae72ffa4a
igorceridorio <igorceridorio@gmail.com>
2023-12-11 14:16:27 -0300

    new solution: Zigzag Conversion

---
+++ solutions/6.zigzag_conversion/zigzag_conversion_test.go
@@ -0,0 +1,9 @@
+package zigzagconversion
+
+import "testing"
+
+func TestConvert(t *testing.T) {
+	println(convert("PAYPALISHIRING", 3))
+	println(convert("PAYPALISHIRING", 4))
+	println(convert("A", 1))
+}
